a8d9bd15b51a40860b59f612da415097cc936f91
mohammedOs1099 <mohammed.osama1099@gmail.com>
2025-04-17 13:23:28 +0200

    � Fix Vercel routing issues & paths

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()
-  ],
-})
+  plugins: [react()],
+  resolve: {
+    
+  },
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    port: 3000
+  }
+});
